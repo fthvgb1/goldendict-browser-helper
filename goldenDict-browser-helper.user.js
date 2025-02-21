9bfdf7e595b1b292e474af0fc279fe98f728f1af
xing <fthvgb1@163.com>
2025-02-21 11:08:05 +0800

    version

--- goldenDict-browser-helper.user.js
+++ goldenDict-browser-helper.user.js
@@ -2,7 +2,7 @@
 // @name         goldenDict-browser-helper
 // @namespace    https://github.com/fthvgb1
 // @homepage     https://github.com/fthvgb1/goldendict-browser-helper
-// @version      1.05
+// @version      1.06
 // @description  调用goldendict
 // @author       https://github.com/fthvgb1
 // @match        http://*/*
