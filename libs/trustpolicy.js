b4ca51f61f6ad16c1feabbf8fa381af1a4213673
xing <fthvgb1@163.com>
2025-04-29 09:42:23 +0800

    add trust policy

---
+++ libs/trustpolicy.js
@@ -0,0 +1,22 @@
+;const {createHtml, createScriptURL, createScript} = (() => {
+    let createHtml = html => html;
+    let createScriptURL = url => url;
+    let createScript = script => script;
+    if (window.trustedTypes && window.trustedTypes.createPolicy) {
+        if (window.trustedTypes.defaultPolicy) {
+            createHtml = html => window.trustedTypes.defaultPolicy.createHTML(html);
+            createScriptURL = s => window.trustedTypes.defaultPolicy.createScriptURL(s);
+            createScript = s => window.trustedTypes.defaultPolicy.createScript(s);
+        } else {
+            window.trustedTypes.createPolicy('default', {
+                createHTML: (string, sink) => string,
+                createScriptURL: s => s,
+                createScript: s => s,
+            });
+            createHtml = html => window.trustedTypes.defaultPolicy.createHTML(html);
+        }
+    }
+    return {
+        createHtml, createScriptURL, createScript
+    }
+});
